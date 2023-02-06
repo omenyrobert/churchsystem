986138d7d946b63f09bdac3034adec3a0f4f2046
MulindwaDenis13 <user@Users-iMac.local>
2023-02-06 15:30:08 +0300

    filter between dates

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -77,22 +77,24 @@ class ReportsController extends Controller
                 $expenses[] = $expense_object;
             }
         }
-        // if (!empty(request()->start_date) && !empty(request()->end_date)) {
-        //     $end_date = request()->end_date;
-        //     $start_date = request()->start_date;
-        //     foreach ($income_types as $income_type) {
-        //         $income = Incomes::where('date', $end_date)->get();
-        //         $total_income_per_type = Incomes::where('date', '>=', $start_date)->where('date', '<=', $end_date)->sum('income');
-        //         $income_object = (object) ['incomes_per_type' => $income, 'total' => $total_income_per_type, 'type' => $income_type->income_type];
-        //         $incomes[] = $income_object;
-        //     }
-        //     foreach ($expense_types as $expense_type) {
-        //         $expense = Expenses::where('date', $end_date)->get();
-        //         $total_expense_per_type = Expenses::where('date', '>=', $start_date)->where('date', $end_date)->sum('expense');
-        //         $expense_object = (object) ['expenses_per_type' => $expense, 'total' => $total_expense_per_type, 'type' => $expense_type->expense_type];
-        //         $expenses[] = $expense_object;
-        //     }
-        // }
+        if (!empty(request()->start_date) && !empty(request()->end_date)) {
+            $end_date = request()->end_date;
+            $start_date = request()->start_date;
+            foreach ($income_types as $income_type) {
+                $income = Incomes::where('date', $end_date)->get();
+                $total_income_per_type = Incomes::whereRaw('STR_TO_DATE(date,"%d-%m-%Y") >= STR_TO_DATE("' . $start_date . '","%d-%m-%Y")')
+                    ->whereRaw('STR_TO_DATE(date,"%d-%m-%Y") <= STR_TO_DATE("' . $end_date . '","%d-%m-%Y")')->sum('income');
+                $income_object = (object) ['incomes_per_type' => $income, 'total' => $total_income_per_type, 'type' => $income_type->income_type];
+                $incomes[] = $income_object;
+            }
+            foreach ($expense_types as $expense_type) {
+                $expense = Expenses::where('date', $end_date)->get();
+                $total_expense_per_type = Expenses::whereRaw('STR_TO_DATE(date,"%d-%m-%Y") >= STR_TO_DATE("' . $start_date . '","%d-%m-%Y")')
+                    ->whereRaw('STR_TO_DATE(date,"%d-%m-%Y") <= STR_TO_DATE("' . $end_date . '","%d-%m-%Y")')->sum('expense');
+                $expense_object = (object) ['expenses_per_type' => $expense, 'total' => $total_expense_per_type, 'type' => $expense_type->expense_type];
+                $expenses[] = $expense_object;
+            }
+        }
         return view('reports.index', compact('expense_types', 'expenses', 'income_types', 'incomes'));
     }
     // git remote add origin https://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@github.com/omenyrobert/churchsystem.git
